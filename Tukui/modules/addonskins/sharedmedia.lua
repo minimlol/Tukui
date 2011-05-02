eb450445e3b5d7634ec0524d8df907b85474c0ca
minim <minim.v@gmail.com>
2011-05-02 22:08:36 +0200

    oops, forgot add files to last commit

---
+++ Tukui/modules/addonskins/sharedmedia.lua
@@ -0,0 +1,9 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+if LSM == nil then return end
+
+LSM:Register("statusbar","Tukui Bar", [[Interface\AddOns\Tukui\medias\textures\normTex.tga]])
+LSM:Register("border", "Tukui Border", [[Interface\AddOns\Tukui\medias\textures\glowTex.tga]])
+LSM:Register("font","Calibri", [[Interface\Addons\Tukui\medias\fonts\calibri.ttf]])
+LSM:Register("sound","Tukui Warning", [[nterface\AddOns\Tukui\medias\sounds\warning.mp3]])
+LSM:Register("sound","Tukui Whisper", [[Interface\AddOns\Tukui\medias\sounds\whisper.mp3]])
