395f45d9b5f50a2d218035ecd7144c50d42a87d1
Tukz <tukz@tukui.org>
2011-12-10 15:54:49 -0500

    Copy URL feature on chat is back, without memory leak.

--- Tukui/modules/chat/copyurl.lua
+++ Tukui/modules/chat/copyurl.lua
@@ -1,66 +1,88 @@
 local T, C, L = unpack(select(2, ...)) -- Import: T - functions, constants, variables; C - config; L - locales
 if C["chat"].enable ~= true then return end
 
-local SetItemRef_orig = SetItemRef
-function ReURL_SetItemRef(link, text, button, chatFrame)
-	if (strsub(link, 1, 3) == "url") then
-		local ChatFrameEditBox = ChatEdit_ChooseBoxForSend()
-		local url = strsub(link, 5);
-		if (not ChatFrameEditBox:IsShown()) then
-			ChatEdit_ActivateChat(ChatFrameEditBox)
-		end
-		ChatFrameEditBox:Insert(url)
-		ChatFrameEditBox:HighlightText()
+local gsub = gsub
+local FindURL = function(self, event, msg, ...)
+	local newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?/%S+", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
 
-	else
-		SetItemRef_orig(link, text, button, chatFrame)
+	newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.[%a%-%d]+%.%a%a%a?/%S+", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?/", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.[%a%-%d]+%.%a%a%a?/", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?$", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "http://[%a%-%d]*%.?[%a%-%d]+%.[%a%-%d]+%.%a%a%a?$", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "(http://[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?) ", "|cff16FF5D|Hurl:%1|h[%1]|h|r ")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "(http://[%a%-%d]*%.?[%a%-%d]+%.[%a%-%d]+%.%a%a%a?) ", "|cff16FF5D|Hurl:%1|h[%1]|h|r ")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?/%S+", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?/", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "[%a%-%d]*%.?[%a%-%d]+%.%a%a%a?$", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "([%a%-%d]*%.?[%a%-%d]+%.%a%a%a?) ", "|cff16FF5D|Hurl:%1|h[%1]|h|r ")
+	if found > 0 then return false, newMsg, ... end
+
+	newMsg, found = gsub(msg, "%d+%.%d+%.%d+%.%d+:?%d*/?%S*", "|cff16FF5D|Hurl:%1|h[%1]|h|r")
+	if found > 0 then return false, newMsg, ... end
+end
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_YELL", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_GUILD", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_OFFICER", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_PARTY_LEADER", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_RAID_LEADER", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_BATTLEGROUND", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_BATTLEGROUND_LEADER", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_SAY", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_WHISPER", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_BN_WHISPER", FindURL)
+ChatFrame_AddMessageEventFilter("CHAT_MSG_BN_CONVERSATION", FindURL)
+
+local currentLink = nil
+local ChatFrame_OnHyperlinkShow_Original = ChatFrame_OnHyperlinkShow
+ChatFrame_OnHyperlinkShow = function(self, link, ...)
+	if (link):sub(1, 3) == "url" then
+		currentLink = (link):sub(5)
+		StaticPopup_Show("TukuiChatCopyUrlPopup")
+		return
 	end
-end
-SetItemRef = ReURL_SetItemRef
-
-function ReURL_AddLinkSyntax(chatstring)
-	if (type(chatstring) == "string") then
-		local extraspace;
-		if (not strfind(chatstring, "^ ")) then
-			extraspace = true;
-			chatstring = " "..chatstring;
-		end
-		chatstring = gsub (chatstring, " www%.([_A-Za-z0-9-]+)%.(%S+)%s?", ReURL_Link("www.%1.%2"))
-		chatstring = gsub (chatstring, " (%a+)://(%S+)%s?", ReURL_Link("%1://%2"))
-		chatstring = gsub (chatstring, " ([_A-Za-z0-9-%.]+)@([_A-Za-z0-9-]+)(%.+)([_A-Za-z0-9-%.]+)%s?", ReURL_Link("%1@%2%3%4"))
-		chatstring = gsub (chatstring, " (%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?):(%d%d?%d?%d?%d?)%s?", ReURL_Link("%1.%2.%3.%4:%5"))
-		chatstring = gsub (chatstring, " (%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%.(%d%d?%d?)%s?", ReURL_Link("%1.%2.%3.%4"))
-		if (extraspace) then
-			chatstring = strsub(chatstring, 2);
-		end
-	end
-	return chatstring
+	ChatFrame_OnHyperlinkShow_Original(self, link, ...)
 end
 
-REURL_COLOR = "16FF5D"
-ReURL_Brackets = false
-ReUR_CustomColor = true
-
-function ReURL_Link(url)
-	if (ReUR_CustomColor) then
-		if (ReURL_Brackets) then
-			url = " |cff"..REURL_COLOR.."|Hurl:"..url.."|h["..url.."]|h|r "
-		else
-			url = " |cff"..REURL_COLOR.."|Hurl:"..url.."|h"..url.."|h|r "
-		end
-	else
-		if (ReURL_Brackets) then
-			url = " |Hurl:"..url.."|h["..url.."]|h "
-		else
-			url = " |Hurl:"..url.."|h"..url.."|h "
-		end
-	end
-	return url
-end
-
---Hook all the AddMessage funcs
-for i=1, NUM_CHAT_WINDOWS do
-	local frame = getglobal("ChatFrame"..i)
-	local addmessage = frame.AddMessage
-	frame.AddMessage = function(self, text, ...) addmessage(self, ReURL_AddLinkSyntax(text), ...) end
-end
+StaticPopupDialogs["TukuiChatCopyUrlPopup"] = {
+	text = "URL",
+	button1 = CLOSE,
+	hasEditBox = 1,
+	editBoxWidth = 350,
+	OnShow = function(frame)
+		frame.editBox:SetText(currentLink)
+		frame.editBox:SetFocus()
+		frame.editBox:HighlightText(0)
+		currentLink = nil
+	end,
+	EditBoxOnEscapePressed = function(frame) frame:GetParent():Hide() end,
+	timeout = 0,
+	whileDead = 1,
+	hideOnEscape = 1,
+	preferredIndex = 3,
+}
